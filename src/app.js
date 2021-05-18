6bfc58426995a6441c082307a13627a4271e4f0e
Akeem Morris <ilswakeem@gmail.com>
2021-05-17 19:54:24 -0500

    added some text

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./header";
+import SecondSection from "./secondsection";
 const App = () => {
  return (
   <main>
    <Header />
+   <SecondSection />
   </main>
  );
 };
---
+++ src/secondsection.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SecondSection = () => {
+ return (
+  <section className="middlesection">
+   <Info />
+  </section>
+ );
+};
+
+function Info() {
+ return (
+  <div className="textarea">
+   <h1> Ship With Confidence</h1>
+   <h2>RocketShip Courier Services</h2>
+  </div>
+ );
+}
+export default SecondSection;
